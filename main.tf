7060e3ce49e071f55a8d3e4d71a5527e2e5ea085
Smi Sinda <sinda.smirani@gmail.com>
2023-02-08 15:39:54 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  backend "s3" {
+    #replace this with your bucket name
+    bucket = "sindasmi-up-and-running-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+
+    #Replace this with your DynamoDB table name!
+    dynamodb_table = "sindasmi-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "sindasmi-up-and-running-state"
+
+  #Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = false
+  }
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "sindasmi-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
